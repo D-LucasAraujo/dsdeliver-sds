973b1d377b02b4376bfa27d98a8b256d3ffa668a
D-LucasAraujo <lucas.jparaujo1@gmail.com>
2021-01-06 19:31:21 -0300

    Post/orders

--- backend/src/main/java/com/lucasaraujo/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/lucasaraujo/dsdeliver/dto/ProductDTO.java
@@ -8,7 +8,7 @@ public class ProductDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private long id;
+	private Long id;
 	private String name;
 	private double price;
 	private String description;
@@ -18,7 +18,7 @@ public class ProductDTO implements Serializable{
 		
 	}
 
-	public ProductDTO(long id, String name, double price, String description, String imageUri) {
+	public ProductDTO(Long id, String name, double price, String description, String imageUri) {
 		this.id = id;
 		this.name = name;
 		this.price = price;
@@ -34,11 +34,11 @@ public class ProductDTO implements Serializable{
 		imageUri = entity.getImageUri();
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- backend/src/main/java/com/lucasaraujo/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/lucasaraujo/dsdeliver/entities/Order.java
@@ -22,7 +22,7 @@ public class Order implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String address;
 	private double latitude;
 	private double longitude;
@@ -40,7 +40,7 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
+	public Order(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
 		super();
 		this.id = id;
 		this.address = address;
@@ -50,11 +50,11 @@ public class Order implements Serializable{
 		this.status = status;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
